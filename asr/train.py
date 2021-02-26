f701d8e4c488856dc6bee722586ccd5db73e068f
hikaruhotta <hikaruho1226@gmail.com>
2021-02-26 05:38:07 +0000

    added train.py

---
+++ asr/train.py
@@ -0,0 +1,38 @@
+from utils import IterMeter
+import os
+from comet_ml import Experiment
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn.functional as F
+import torchaudio
+import numpy as np
+
+
+def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment):
+    model.train()
+    data_len = len(train_loader.dataset)
+    for batch_idx, _data in enumerate(train_loader):
+        spectrograms, labels, input_lengths, label_lengths = _data 
+        spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        output = model(spectrograms)  # (batch, time, n_class)
+        output = F.log_softmax(output, dim=2)
+        output = output.transpose(0, 1) # (time, batch, n_class)
+
+        loss = criterion(output, labels, input_lengths, label_lengths)
+        loss.backward()
+
+        experiment.log_metric('loss', loss.item(), step=iter_meter.get())
+        experiment.log_metric('learning_rate', scheduler.get_lr(), step=iter_meter.get())
+
+        optimizer.step()
+        scheduler.step()
+        iter_meter.step()
+        if batch_idx % 100 == 0 or batch_idx == data_len:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(spectrograms), data_len,
+                100. * batch_idx / len(train_loader), loss.item()))
---
+++ asr/utils.py
@@ -0,0 +1,10 @@
+class IterMeter(object):
+    """keeps track of total iterations"""
+    def __init__(self):
+        self.val = 0
+
+    def step(self):
+        self.val += 1
+
+    def get(self):
+        return self.val
