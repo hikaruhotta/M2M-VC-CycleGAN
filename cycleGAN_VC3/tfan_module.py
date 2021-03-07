de3e7c178bea91b1243714a9fb111a3e8cacfb0f
Sofian Zalouk <sofian.zalouk@mail.utoronto.ca>
2021-03-07 07:52:46 +0000

    Refactoring TFAN module code

--- cycleGAN_VC3/tfan_module.py
+++ cycleGAN_VC3/tfan_module.py
@@ -1,8 +1,4 @@
-#! python
-# -*- coding: utf-8 -*-
-# Author: kun
-# @Time: 2020-11-17 14:35
-
+# Code based on 
 import re
 import torch
 import torch.nn as nn
@@ -37,65 +33,51 @@ def get_norm_layer(opt):
 
 class TFAN_1D(nn.Module):
     """
-    as paper said, it has best performance when N=3, kernal_size in h is 5
+    Implementation follows CycleGAN-VC3 paper: 
+    Parameter choices for number of layers N=3, kernel_size in h is 5
     """
 
-    def __init__(self, norm_nc, ks=5, label_nc=128, N=3):
+    def __init__(self, norm_nc, kernel_size=5, label_nc=80, N=3):
         super().__init__()
 
         self.param_free_norm = nn.InstanceNorm1d(norm_nc, affine=False)
 
-        self.repeat_N = N
+        hidden_size = 128
+        padding = kernel_size // 2
 
-        # The dimension of the intermediate embedding space. Yes, hardcoded.
-        nhidden = 128
+        mlp_layers = [nn.Conv1d(label_nc, hidden_size, kernel_size=kernel_size, padding=padding), nn.ReLU()]
+        for i in range(N - 1):
+            mlp_layers += [nn.Conv1d(hidden_size, hidden_size, kernel_size=kernel_size, padding=padding), nn.ReLU()]
 
-        pw = ks // 2
-
-        self.mlp_shared = nn.Sequential(
-            nn.Conv1d(80, nhidden, kernel_size=ks, padding=pw),
-            nn.ReLU(),
-            nn.Conv1d(nhidden, nhidden, kernel_size=ks, padding=pw),
-            nn.ReLU(),
-            nn.Conv1d(nhidden, nhidden, kernel_size=ks, padding=pw),
-            nn.ReLU()
-        )
-        print("norm_nc: ", norm_nc)
-        self.mlp_gamma = nn.Conv1d(nhidden, norm_nc, kernel_size=ks, padding=pw)
-        self.mlp_beta = nn.Conv1d(nhidden, norm_nc, kernel_size=ks, padding=pw)
+        self.mlp_shared = nn.Sequential(*mlp_layers)
+        self.mlp_gamma = nn.Conv1d(hidden_size, norm_nc, kernel_size=kernel_size, padding=padding)
+        self.mlp_beta = nn.Conv1d(hidden_size, norm_nc, kernel_size=kernel_size, padding=padding)
 
     def forward(self, x, segmap):
-        # Part 1. generate parameter-free normalized activations
+        # Step 1. Instance normalization of features
         normalized = self.param_free_norm(x)
 
-        print("Before TFAN interpolation")
-        print(segmap.shape, x.shape)
-        # Part 2. produce scaling and bias conditioned on semantic map
-        resize_shape = list(segmap.shape)
-        resize_shape[-1] = x.size()[2]
-        # resize_shape[-2] = 128
-        resize_shape = tuple(resize_shape)
-        segmap = F.interpolate(segmap, size=resize_shape[2:], mode='nearest')
-        segmap = segmap.squeeze(1)
-        print(segmap.shape)
+        # print("Before TFAN interpolation")
+        # print(segmap.shape, x.shape)
 
-        # actv = self.mlp_shared(segmap)
-        temp = segmap
-        # for i in range(self.repeat_N):
-        temp = self.mlp_shared(temp)
-            
-        actv = temp
-        
-        print("actv: ", actv.shape)
+        # Step 2. Generate scale and bias conditioned on semantic map
+        Bx, _, Cx, Tx = segmap.shape
+        Bx, Cf, Tf = x.shape
+        segmap = F.interpolate(segmap, size=(Cx, Tf), mode='nearest')
+        segmap = segmap.squeeze(1)
+        # print(segmap.shape)
+
+        actv = self.mlp_shared(segmap)
+        # print("actv: ", actv.shape)
 
         gamma = self.mlp_gamma(actv)
-        print("gamma: ", gamma.shape)
+        # print("gamma: ", gamma.shape)
         beta = self.mlp_beta(actv)
-        print("beta: ", beta.shape)
+        # print("beta: ", beta.shape)
 
         # apply scale and bias
         out = normalized * (1 + gamma) + beta
-        print(out.shape)
+        # print(out.shape)
         return out
 
 
@@ -130,16 +112,16 @@ class TFAN_2D(nn.Module):
         # Part 1. generate parameter-free normalized activations
         normalized = self.param_free_norm(x)
 
-        print("normalized: ", normalized.shape)
-        print(segmap.shape)
-        print(x.shape)
+        # print("normalized: ", normalized.shape)
+        # print(segmap.shape)
+        # print(x.shape)
         resize_shape = list(segmap.shape)
         resize_shape[-2] = x.shape[2]
         resize_shape[-1] = x.shape[-1]
         resize_shape = tuple(resize_shape)
         # Part 2. produce scaling and bias conditioned on semantic map
         segmap = F.interpolate(segmap, size=resize_shape[2:], mode='nearest')
-        print("shape after interpolate: ", segmap.shape)
+        # print("shape after interpolate: ", segmap.shape)
         # actv = self.mlp_shared(segmap)
         temp = segmap
         # for i in range(self.repeat_N):
