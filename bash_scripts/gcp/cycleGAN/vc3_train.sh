876591773d855b0cd9c4328573d355be16e8ea32
hikaruhotta <hikaruho1226@gmail.com>
2021-03-08 05:14:58 +0000

    stashing

--- bash_scripts/gcp/cycleGAN/vc3_train.sh
+++ bash_scripts/gcp/cycleGAN/vc3_train.sh
@@ -1,11 +1,11 @@
 python -W ignore::UserWarning -m cycleGAN_VC3.train \
     --name debug \
     --save_dir /home/results/cycleGAN_VC3 \
-    --num_epochs 30 \
+    --num_epochs 50 \
     --normalized_dataset_A_path /home/data/vc3_melspec_dataset/voc_normalized.pickle \
     --norm_stats_A_path /home/data/vc3_melspec_dataset/norm_stat_voc.npz \
     --normalized_dataset_B_path /home/data/vc3_melspec_dataset/coraal_normalized.pickle \
     --norm_stats_B_path /home/data/vc3_melspec_dataset/norm_stat_coraal.npz \
-    --epochs_per_save 1 \
-    --epochs_per_plot 10 \
+    --epochs_per_save 50 \
+    --epochs_per_plot 5 \
     # --continue_train
