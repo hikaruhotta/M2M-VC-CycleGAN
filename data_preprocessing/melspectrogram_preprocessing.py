4f9c022739a4ebb8634af88195394df29680f893
Sofian Zalouk <sofian.zalouk@mail.utoronto.ca>
2021-03-09 08:42:08 +0000

    Generated new preprocessed dataset, training CycleGAN VC3 now

--- data_preprocessing/melspectrogram_preprocessing.py
+++ data_preprocessing/melspectrogram_preprocessing.py
@@ -30,54 +30,7 @@ mel_fmax = None
 
 # Code from: https://github.com/descriptinc/melgan-neurips
 
-
-class Audio2Mel(nn.Module):
-    def __init__(
-        self,
-        n_fft=1024,
-        hop_length=256,
-        win_length=1024,
-        sampling_rate=22050,
-        n_mel_channels=80,
-        mel_fmin=0.0,
-        mel_fmax=None,
-    ):
-        super().__init__()
-        ##############################################
-        # FFT Parameters                              #
-        ##############################################
-        window = torch.hann_window(win_length).float()
-        mel_basis = librosa_mel_fn(
-            sampling_rate, n_fft, n_mel_channels, mel_fmin, mel_fmax
-        )
-        mel_basis = torch.from_numpy(mel_basis).float()
-        self.register_buffer("mel_basis", mel_basis)
-        self.register_buffer("window", window)
-        self.n_fft = n_fft
-        self.hop_length = hop_length
-        self.win_length = win_length
-        self.sampling_rate = sampling_rate
-        self.n_mel_channels = n_mel_channels
-
-    def forward(self, audio):
-        p = (self.n_fft - self.hop_length) // 2
-        audio = F.pad(audio, (p, p), "reflect").squeeze(1)
-        fft = torch.stft(
-            audio,
-            n_fft=self.n_fft,
-            hop_length=self.hop_length,
-            win_length=self.win_length,
-            window=self.window,
-            center=False,
-        )
-        real_part, imag_part = fft.unbind(-1)
-        magnitude = torch.sqrt(real_part ** 2 + imag_part ** 2)
-        mel_output = torch.matmul(self.mel_basis, magnitude)
-        log_mel_spec = torch.log10(torch.clamp(mel_output, min=1e-5))
-        return log_mel_spec
-
-
-def normels(wavspath):
+def normalize_mel(wavspath):
     wav_files = glob.glob(os.path.join(
         wavspath, '**', '*.wav'), recursive=True)  # source_path
     vocoder = torch.hub.load('descriptinc/melgan-neurips', 'load_melgan')
@@ -114,8 +67,8 @@ def buildTrainset(source_path, target_path, cache_folder='./cache/'):
 
     print('Building training dataset...')
 
-    mel_normalized_A, mel_mean_A, mel_std_A = normels(source_path)
-    mel_normalized_B, mel_mean_B, mel_std_B = normels(target_path)
+    mel_normalized_A, mel_mean_A, mel_std_A = normalize_mel(source_path)
+    mel_normalized_B, mel_mean_B, mel_std_B = normalize_mel(target_path)
 
     if not os.path.exists(cache_folder):
         os.makedirs(cache_folder)
